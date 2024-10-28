a3b0d1e4a7c777b91277850c54340b6323cc4602
Sanika Bhamare <sanikabhamare@gmail.com>
2024-10-27 22:50:47 -0700

    Qr code genrator frame built

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,45 @@
 import "./App.css";
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const apiCall = () => {
-    axios.get("http://localhost:3000").then((data) => {
-      console.log(data);
-    });
+  const [url, setURL] = useState(" ");
+  const [qrCode, setQRCode] = useState(null);
+
+  const generateQRCode = async () => {
+    if (!url.trim()) {
+      // Check if URL is empty or just whitespace
+      console.error("URL is required");
+      return;
+    }
+    try {
+      const response = await axios.post("http://localhost:3000/generate", {
+        url,
+      });
+      setQRCode(response.data.qrCode);
+    } catch (err) {
+      console.error("Error generating code", err);
+    }
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={apiCall}>Make API Call</button>
+        <h1>QR CODE GENERATOR</h1>
+        <input
+          type="text"
+          placeholder="Enter URL"
+          value={url}
+          onChange={(e) => setURL(e.target.value)}
+        />
+
+        <button onClick={generateQRCode}>Generate</button>
+        {qrCode && (
+          <div>
+            <h3>Generated QR Code</h3>
+            <img src={qrCode} alt={url} />
+          </div>
+        )}
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const QRCode = require("qrcode");
 
+const app = express();
 app.use(cors());
+app.use(express.json());
 
 // this is the home page
-app.get("/", (req, res) => {
-  res.send("QRCoder");
+app.get("/", (res) => {
+  res.send("QRCoder server");
+});
+
+//setting up the genrate route
+app.post("/generate", (req, res) => {
+  const { url } = req.body;
+  if (!url) {
+    return res.status(400).send("Enter a URL");
+  }
+
+  QRCode.toDataURL(url, (err, qrCode) => {
+    if (err) {
+      return res.status(500).send("Failed to generate qrcode");
+    }
+    res.json({ qrCode });
+  });
 });
 
 app.listen(3000, () => {
